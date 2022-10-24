754442bbcc8e1e2b5914e99a2a5024ae9875aed8
54ycf <1124833602@qq.com>
2022-10-24 21:30:02 +0800

    547 needs to think for a while

---
+++ 547.省份数量.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode.cn id=547 lang=java
+ *
+ * [547] 省份数量
+ */
+
+// @lc code=start
+class Solution {
+    int[][] isConnected;
+    int n;
+    boolean[] visited;
+    public int findCircleNum(int[][] isConnected) {
+        this.isConnected = isConnected;
+        this.n = isConnected.length; //the number of cites
+        this.visited = new boolean[n];
+        int result = 0; //number of provinces
+        for(int i=0; i<n; ++i){
+            if(!visited[i]){
+                dfs(i);
+                ++result;
+            }
+        }
+        return result;
+    }
+
+    public void dfs(int curCity){
+        int[] line = isConnected[curCity];
+        for(int i=0; i<n; ++i){
+            if(!visited[i] && line[i]==1){
+                visited[i] = true;
+                dfs(i);
+            }
+        }
+    }
+}
+// @lc code=end
+
