6a329adcbc28ef105e9de0750a4653d4dcc27f0f
54ycf <1124833602@qq.com>
2022-10-29 17:18:11 +0800

    Theoretically optimal performance

--- 973.最接近原点的-k-个点.java
+++ 973.最接近原点的-k-个点.java
@@ -1,4 +1,5 @@
 import java.util.PriorityQueue;
+import java.util.Random;
 
 /*
  * @lc app=leetcode.cn id=973 lang=java
@@ -9,26 +10,47 @@ import java.util.PriorityQueue;
 // @lc code=start
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        //max heap
-        PriorityQueue<int[]> heap = new PriorityQueue<int[]>( (arr1,arr2) -> arr2[0]-arr1[0] ); 
-        for(int i=0; i<k; ++i){ //init heap by size k of heap
-            int d = points[i][0]*points[i][0] + points[i][1]*points[i][1];
-            heap.offer(new int[]{d,i}); //distance and pointer
-        }
         int n = points.length;
-        for(int i=k; i<n; ++i){
+        int[][] arr = new int[n][2]; //not necessary, if use points[][], more efficient
+        for(int i=0; i<n; ++i){
             int d = points[i][0]*points[i][0] + points[i][1]*points[i][1];
-            if(heap.peek()[0] > d){ //always be the min k
-                heap.poll();
-                heap.offer(new int[]{d,i});
+            arr[i][0] = d;
+            arr[i][1] = i;
+        }
+        selectK(arr, k, 0, n-1);
+        int[][] ans = new int[k][2];
+        for(int i=0; i<k; ++i){
+            ans[i] = points[arr[i][1]];
+        }
+        return ans;
+    }
+
+    Random random = new Random();
+    void selectK(int[][] arr, int k, int l, int r){
+        int pPivot = l + random.nextInt(r-l+1);
+        //partition ->
+        swap(arr, pPivot, r);
+        int i=l, j=r-1;
+        int pivotVal = arr[r][0];
+        while(i-1 != j){ //beware not i!=j
+            if(arr[i][0] <= pivotVal){
+                ++i;
+            }else{
+                swap(arr, i, j);
+                --j;
             }
         }
-        int[][] res = new int[k][2];
-        int i=0;
-        for(int[] item : heap){
-            res[i++] = points[item[1]];
-        }
-        return res;
+        swap(arr, i, r);
+        //<- partition
+        if(i == k - 1) return;
+        else if(i < k - 1) selectK(arr, k, i+1, r);
+        else selectK(arr, k, l, i-1);
+    }
+
+    void swap(int[][] arr, int i, int j){
+        int[] tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
 // @lc code=end
