b3a043e11679892eadd348921fb923fcabda573f
54ycf <1124833602@qq.com>
2023-03-02 21:30:15 +0800

    3 自己写的代码但是太慢，质量不高

--- 3.无重复字符的最长子串.java
+++ 3.无重复字符的最长子串.java
@@ -1,3 +1,8 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /*
  * @lc app=leetcode.cn id=3 lang=java
  *
@@ -7,7 +12,28 @@
 // @lc code=start
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        return 1;
+        int len = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0; //子串开始的位置
+        int max = 0;
+        for (int i = 0; i < len; i++) {
+            Character ch = s.charAt(i);
+            if(map.containsKey(ch)){
+                int ind = map.get(ch); //第一个重复的位置
+                for (int j = left; j <= ind; j++) {
+                    map.remove(s.charAt(j));
+                }
+                map.put(ch, i);
+                int tmp = i-left;
+                max = max > tmp? max:tmp;
+                left = ind+1;
+            }else{
+                map.put(ch, i);
+            }
+        }
+        int tmp = len - left;
+        max = max > tmp? max:tmp;
+        return max;
     }
 }
 // @lc code=end
