18d0408e4c44c97d3caae1d0938bdca9f7f24c6d
tony <1124833602@qq.com>
2023-03-28 18:38:28 +0800

    经典！

---
+++ 1143.最长公共子序列.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=java
+ *
+ * [1143] 最长公共子序列
+ */
+
+// @lc code=start
+class Solution {
+    /*
+     * dp[i][j]代表text1的0～i和text2的0～j的最长公共子序列
+     * 如果i和j两个位置两个字符相等，则最长是i-1、j-1的最长+1，
+     * 否则就是i～j-1和i-1～j
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                } else {
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+// @lc code=end
